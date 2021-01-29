e1fe648b4390c89bcd296950e54d115ae1bdc97b
Sven Kraus <mail@svenkraus.de>
2021-01-29 15:15:33 +0100

    refactor path drawing to a separate component

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-import {getLocationSetByCharacter, getPlaceData, getPlaceLocation} from "./data"
-import type {LocationSet, CharacterName, DateKey, PlaceLocation, PlaceName} from "./data"
+import CharacterLayer from "./CharacterLayer.svelte"
 let svg;
 let showCoords: boolean = false;
 function clicked (e){
@@ -15,92 +14,18 @@ function clicked (e){
 
 	console.log(svgP.x)
 	console.log(svgP.y)
-	if(showCoords)
-		alert("x: "+svgP.x+" y:"+svgP.y);
-}
-let drawBase: Node;
-function drawCircle(x: number, y: number) {
-	console.log("drawnow")
-	var rect = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
-	rect.setAttributeNS(null, 'width', 100)
-	rect.setAttributeNS(null, 'cx', x.toString())
-	rect.setAttributeNS(null, 'cy', y.toString())
-	// rect.setAttributeNS(null, 'cx', 855)
-	// rect.setAttributeNS(null, 'cy', 741)
-
-	rect.setAttributeNS(null, 'r', "6")
-	rect.setAttributeNS(null, 'fill', 'red')
-	drawBase.appendChild(rect)
-
-}
-function drawLine(loc1: PlaceLocation, loc2: PlaceLocation, withDots=true) {
-	var line = document.createElementNS("http://www.w3.org/2000/svg", 'line')
-	//  <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
-	line.setAttributeNS(null, 'x1', loc1.x.toString())
-	line.setAttributeNS(null, 'y1', loc1.y.toString())
-	line.setAttributeNS(null, 'x2', loc2.x.toString())
-	line.setAttributeNS(null, 'y2', loc2.y.toString())
-	line.setAttributeNS(null, 'stroke', "red")
-	line.setAttributeNS(null, 'stroke-width', "3")
-	// line.setAttributeNS(null, 'cx', 855)
-	// line.setAttributeNS(null, 'cy', 741)
-	drawBase.appendChild(line)
-	if(withDots){
-		drawCircle(loc1.x, loc1.y)
-		drawCircle(loc2.x, loc2.y)
-
+	if(showCoords){
+		const text = `"x": ${Math.round(parseFloat(svgP.x )*100 ) / 100 },\n"y": ${Math.round(parseFloat(svgP.y) * 100) / 100}`
+		navigator.clipboard.writeText(text).then(() => {console.log("copied to clipboard")})
+		alert(text);
 	}
 }
-function drawLineFromPlaces(place1: PlaceName, place2: PlaceName ) {
-	const promises = [getPlaceLocation(place1)]
-	promises.push(getPlaceLocation(place2))
-	Promise.all(promises).then(locations => {
-		const loc1 = locations[0]
-		const loc2 = locations[1]
-		drawLine(loc1, loc2)
-	})
-}
-function cleanDrawings() {
-	Array.from(drawBase.childNodes).forEach(child => {
-		drawBase.removeChild(child)
-	});
-}
-let availableDates = [undefined]
+
 let character = "Frodo"
 const availableChars = ["Frodo"]
-let characterData:Map<CharacterName, LocationSet> = new Map()
-getLocationSetByCharacter(character).then((data: LocationSet) => {
-	characterData = characterData.set(character, data);
-	console.log("CharData:", characterData);
-	availableDates = characterData.has(character) ? Array.from(characterData.get(character).keys()) : [undefined]
-})
-function getLocName(date: string): string {
-	if(date)
-		return characterData.get(character).get(date)
-	else
-		return ""
-}
+let availableDates = [undefined]
 let includePath: boolean = true;
-let dateIndex = 0 
-$: currentLocationName = getLocName(availableDates[dateIndex])
-
-$: getPlaceLocation(currentLocationName).then(loc => {
-	if(loc) {
-		cleanDrawings()
-		if(includePath && dateIndex > 0) {
-			let lastLoc = currentLocationName;
-			//We neeed to draw a path between all previous ones.
-			availableDates.slice(0,dateIndex).reverse().forEach(date => {
-				const tmp = getLocName(date)
-				drawLineFromPlaces(lastLoc,tmp);
-				lastLoc = tmp;
-			});
-		}
-		else {
-			drawCircle(loc.x,loc.y)
-		}
-	}
-})
+let dateIndex: number = 0;
 </script>
 <header>
 <div id="header">
@@ -113,11 +38,11 @@ $: getPlaceLocation(currentLocationName).then(loc => {
 			<option value="{char}">{char}</option>
 			{/each}
 		</select>
-		<label for="loc">Latest Location:</label><span id="loc" name="loc">{currentLocationName}</span>
-		{#if characterData.size > 0 && characterData.get(character).size > 0}
+		<label for="loc">Latest Location:</label><span id="loc" name="loc">TODO</span>
+		{#if availableDates.length > 0 }
 		<label for="date-index">Date: {availableDates[dateIndex]}</label>
 			<input type="range" id="date-index" name="date-index"
-			min="0" max="{characterData.get(character).size -1}" bind:value={dateIndex} >
+			min="0" max="{availableDates.length -1}" bind:value={dateIndex} >
 				<label for="include-path">draw Path?</label>
 				<input type="checkbox" name="include-path" bind:checked={includePath}>
 		{/if}
@@ -130,7 +55,7 @@ $: getPlaceLocation(currentLocationName).then(loc => {
 	<svg id="map" viewBox="0 0 3200 2400" version="1.1"
 	 xmlns="http://www.w3.org/2000/svg" on:click={clicked}  bind:this={svg}>
 		 <image x="0" y="0" href="/img/mapome-slim.svg" width="100%" height="100%" />
-		 <g id="drawing"bind:this={drawBase}></g>
+		 <CharacterLayer {character} {includePath} bind:availableDates={availableDates} {dateIndex} />
 </svg>
 
 	<!-- <object data="/img/mapome-slim.svg" type="image/svg+xml"  > -->
---
+++ app/src/CharacterLayer.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+    import {getLocationSetByCharacter, getPlaceData, getPlaceLocation} from "./data"
+    import type {LocationSet, CharacterName, DateKey, PlaceLocation, PlaceName} from "./data"
+    export let character ="Frodo"
+    export let includePath: boolean = true;
+
+    export let availableDates //TODO: later i will not export and bind this anymore, as i display all dates?
+    export let dateIndex = 0;
+    let characterData:Map<CharacterName, LocationSet> = new Map()
+    let drawBase;
+    getLocationSetByCharacter(character).then((data: LocationSet) => {
+	characterData = characterData.set(character, data);
+	console.log("CharData:", characterData);
+	availableDates = characterData.has(character) ? Array.from(characterData.get(character).keys()) : [undefined]
+    })
+
+    function drawCircle(x: number, y: number) {
+	console.log("drawnow")
+	var rect = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
+	rect.setAttributeNS(null, 'width', 100)
+	rect.setAttributeNS(null, 'cx', x.toString())
+	rect.setAttributeNS(null, 'cy', y.toString())
+	// rect.setAttributeNS(null, 'cx', 855)
+	// rect.setAttributeNS(null, 'cy', 741)
+
+	rect.setAttributeNS(null, 'r', "6")
+	rect.setAttributeNS(null, 'fill', 'red')
+	drawBase.appendChild(rect)
+
+}
+function drawLine(loc1: PlaceLocation, loc2: PlaceLocation, withDots=true) {
+	var line = document.createElementNS("http://www.w3.org/2000/svg", 'line')
+	//  <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+	line.setAttributeNS(null, 'x1', loc1.x.toString())
+	line.setAttributeNS(null, 'y1', loc1.y.toString())
+	line.setAttributeNS(null, 'x2', loc2.x.toString())
+	line.setAttributeNS(null, 'y2', loc2.y.toString())
+	line.setAttributeNS(null, 'stroke', "red")
+	line.setAttributeNS(null, 'stroke-width', "3")
+	// line.setAttributeNS(null, 'cx', 855)
+	// line.setAttributeNS(null, 'cy', 741)
+	drawBase.appendChild(line)
+	if(withDots){
+		drawCircle(loc1.x, loc1.y)
+		drawCircle(loc2.x, loc2.y)
+
+	}
+}
+function drawLineFromPlaces(place1: PlaceName, place2: PlaceName ) {
+	const promises = [getPlaceLocation(place1)]
+	promises.push(getPlaceLocation(place2))
+	Promise.all(promises).then(locations => {
+		const loc1 = locations[0]
+		const loc2 = locations[1]
+		drawLine(loc1, loc2)
+	})
+}
+function cleanDrawings() {
+	Array.from(drawBase.childNodes).forEach(child => {
+		drawBase.removeChild(child)
+	});
+}
+
+function getLocName(date: string): PlaceName {
+	if(date)
+		return characterData.get(character).get(date)
+	else
+		return ""
+}
+$: currentLocationName = getLocName(availableDates[dateIndex])
+
+$: getPlaceLocation(currentLocationName).then(loc => {
+	if(loc) {
+		cleanDrawings()
+		if(includePath && dateIndex > 0) {
+			let lastLoc = currentLocationName;
+			//We neeed to draw a path between all previous ones.
+			availableDates.slice(0,dateIndex).reverse().forEach(date => {
+				const tmp = getLocName(date)
+				drawLineFromPlaces(lastLoc,tmp);
+				lastLoc = tmp;
+			});
+		}
+		else {
+			drawCircle(loc.x,loc.y)
+		}
+	}
+})
+</script>
+
+<g id="drawing"bind:this={drawBase}></g>
