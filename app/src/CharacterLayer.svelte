652e8a7ab157c23ba3df142656a20466d46b7216
Sven Kraus <mail@svenkraus.de>
2021-02-01 16:27:30 +0100

    switch to d3 for drawing

--- app/src/CharacterLayer.svelte
+++ app/src/CharacterLayer.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
+	import * as d3 from "d3";
+	/*
+	TODOS: 
+	- add tooltip to dot´s showing Name: Location // Maybe switch to d3.js for svg drawing
+	- hide controls and make them a bit nicer
+	- Display Event Text
+	- Add (Starting positions to characters?)
+	- add smooth Paths
+	*/
     import {getLocationSetByCharacter, getPlaceData, getPlaceLocation} from "./data"
-    import type {LocationSet, CharacterName, DateKey, PlaceLocation, PlaceName} from "./data"
-import { ShireDate } from "./data/util";
+   import type {LocationSet, CharacterName, DateKey, PlaceLocation, PlaceName} from "./data"
+ import { ShireDate } from "./data/util";
     export let character ="Frodo"
     export let includePath: boolean = true;
 	export let selectedDate = ""
 	export let color = "red"
-
-	console.log(`DIsplay Path for ${character} at Date ${selectedDate} in Color ${color} (includepahts: ${includePath})`)
+	let popovers = []
+	console.log(`Display Path for ${character} at Date ${selectedDate} in Color ${color} (includepahts: ${includePath})`)
     // export let dateIndex = 0;
     let characterData:Map<CharacterName, LocationSet> = new Map()
 	let drawBase;
+	
 	function initCharacterData() {
 		getLocationSetByCharacter(character).then((data: LocationSet) => {
 		characterData = characterData.set(character, data);
@@ -20,38 +30,35 @@ import { ShireDate } from "./data/util";
 		})
 	}
     $: character, initCharacterData()
-
-    function drawCircle(x: number, y: number) {
+    function drawCircle(x: number, y: number, name="") {
 		console.log("drawnow")
-		var rect = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
-		rect.setAttributeNS(null, 'width', 100)
-		rect.setAttributeNS(null, 'cx', x.toString())
-		rect.setAttributeNS(null, 'cy', y.toString())
-		// rect.setAttributeNS(null, 'cx', 855)
-		// rect.setAttributeNS(null, 'cy', 741)
-
-		rect.setAttributeNS(null, 'r', "6")
-		rect.setAttributeNS(null, 'fill', color)
-		drawBase.appendChild(rect)
+		const d3Base = d3.select(drawBase)
+		d3Base.append("circle")
+			.style("fill", color)
+			.attr("r", 6)
+			.attr("cx", x)
+			.attr("cy", y)
+			.attr("width", 100);
 
 }
-function drawLine(loc1: PlaceLocation, loc2: PlaceLocation, withDots=true) {
-	var line = document.createElementNS("http://www.w3.org/2000/svg", 'line')
-	//  <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
-	line.setAttributeNS(null, 'x1', loc1.x.toString())
-	line.setAttributeNS(null, 'y1', loc1.y.toString())
-	line.setAttributeNS(null, 'x2', loc2.x.toString())
-	line.setAttributeNS(null, 'y2', loc2.y.toString())
-	line.setAttributeNS(null, 'stroke', color)
-	line.setAttributeNS(null, 'stroke-width', "3")
-	// line.setAttributeNS(null, 'cx', 855)
-	// line.setAttributeNS(null, 'cy', 741)
-	drawBase.appendChild(line)
-	if(withDots){
-		drawCircle(loc1.x, loc1.y)
-		drawCircle(loc2.x, loc2.y)
 
-	}
+
+function drawLine(loc1: PlaceLocation, loc2: PlaceLocation, withDots=true) {
+
+	const d3Base = d3.select(drawBase)
+	d3Base.append("line")
+		.style("stroke", color)
+		.attr("x1", loc1.x)
+		.attr("x2", loc2.x)
+		.attr("y1", loc1.y)
+		.attr("y2", loc2.y)
+		.attr("stroke-width", 3);
+		if(withDots){
+			drawCircle(loc1.x, loc1.y)
+			drawCircle(loc2.x, loc2.y)
+			
+		}
+		
 }
 function drawLineFromPlaces(place1: PlaceName, place2: PlaceName ) {
 	const promises = [getPlaceLocation(place1)]
@@ -88,14 +95,14 @@ function getAllPreviousLocationDates(date: string, includeCurrent=true): string[
 	if(!date || date == "")
 		return []
 	const availableDates = characterData.has(character) ? Array.from(characterData.get(character).keys()) : []
-	console.log("COnverting date", date)
+	console.log("Converting date", date)
 	const shireDate = new ShireDate(date)
 	console.log("availabelDates", availableDates)
 	const locs = availableDates.filter(
 		item => shireDate.compareTo(new ShireDate(item)) >= 0
 	).sort(
 		(a,b) => (new ShireDate(a)).compareTo(new ShireDate(b))
-		)
+		).reverse()
 	return includeCurrent ? locs : locs.splice(1)
 }
 
@@ -113,10 +120,9 @@ $: getPlaceLocation(currentLocationName).then(loc => {
 			//We neeed to draw a path between all previous ones.
 		}
 		else {
-			drawCircle(loc.x,loc.y)
+			drawCircle(loc.x,loc.y, currentLocationName)
 		}
 	}
 })
 </script>
-
 <g id="path-{character.toLowerCase()}" bind:this={drawBase}></g>
